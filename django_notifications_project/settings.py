ccf8eaa48219ee8fe6aba15c299dc6be433316d3
Huynh HD <honghuynhit@gmail.com>
2020-10-23 14:51:06 +0700

    Update settings.py

--- django_notifications_project/settings.py
+++ django_notifications_project/settings.py
@@ -137,7 +137,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_rabbitmq.core.RabbitmqChannelLayer",
         "CONFIG": {
-            "host": "amqp://admin:StrongPassword@1270.0.0.1/",
+            "host": "amqp://test:test@139.180.205.6/",
         },
     },
 }
